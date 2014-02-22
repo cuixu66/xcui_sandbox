81e30499b516c74f5dac5811856e9991c9fde675
xcui <cuixu66@hotmail.com>
2014-02-22 13:37:20 -0500

    modified missed bar script so it now only display client # 96 and 192 (removed client #s 384 & 768), also removed the confusing aggregation of miss + reject

--- python/MicroFuge_Pyplot/bars/miss_bar.py
+++ python/MicroFuge_Pyplot/bars/miss_bar.py
@@ -13,16 +13,16 @@ HOME_DIR = expanduser("~")
 PATTERN = [ "/" , "\\" ,"o", "*", "O", "x", ".", '-', "|" , "+" ]
 
 MM_DIR = HOME_DIR + "/ICDCS/ICDCS_DATA/EC2" + "/EC2_mm_v1/test/"
-MM_CLIENT_NUMBERS = [24,48,96,192]
+MM_CLIENT_NUMBERS = [24,48] # [24,48,96,192]
 
 CS_DIR = HOME_DIR + "/ICDCS/ICDCS_DATA/EC2" + "/cs/test/"
-CS_CLIENT_NUMBERS = [24,48,96,192]
+CS_CLIENT_NUMBERS = [24,48] # [24,48,96,192]
 SH_DIR = HOME_DIR + "/ICDCS/ICDCS_DATA/EC2" + "/sh/no_ac/"
-SH_CLIENT_NUMBERS = [24,48,96,192]
+SH_CLIENT_NUMBERS = [24,48] # [24,48,96,192]
 AC_DIR = HOME_DIR + "/ICDCS/ICDCS_DATA/EC2" + "/sh/ac/"
-AC_CLIENT_NUMBERS = [24,48,96,192]
+AC_CLIENT_NUMBERS = [24,48] # [24,48,96,192]
 DN_DIR = HOME_DIR + "/ICDCS/ICDCS_DATA/EC2" + "/dn/test/"
-DN_CLIENT_NUMBERS = [24,48,96,192]
+DN_CLIENT_NUMBERS = [24,48] # [24,48,96,192]
 CS_RW_DIR = HOME_DIR + "/ICDCS/ICDCS_DATA/EC2" + "/rw/cs_rw/"
 CS_RW_CLIENT_NUMBERS = [24,48]
 SH_RW_DIR = HOME_DIR + "/ICDCS/ICDCS_DATA/EC2" + "/rw/sh_rw/"
@@ -67,7 +67,8 @@ def get_results(data_dir, client_nums, rtn_mean, rtn_std, ac_huh = False, reject
         rtn_std.append(stdmean_stderr[1])
 
 
-N = 4
+# N = 4
+N = 2
 ind = np.arange(N)    # the x locations for the groups
 width = 0.11      # the width of the bars: can also be len(x) sequence
 
@@ -82,76 +83,90 @@ plt.figure(None, figsize=(8,4), dpi = 100)
 mean_values = []
 stderr_values = []
 get_results(DN_DIR, DN_CLIENT_NUMBERS, mean_values, stderr_values)
-p0 = plt.bar(ind + width * 0,  mean_values,width, color='w', yerr=stderr_values,
+p0 = plt.bar(ind + width * 0,  mean_values,width, color='w',
+             yerr=stderr_values, ecolor='r',
              edgecolor='black', hatch=PATTERN[0])
 
 
 mean_values = []
 stderr_values = []
 get_results(MM_DIR, MM_CLIENT_NUMBERS, mean_values, stderr_values)
-p1 = plt.bar(ind + width * 1, mean_values, width, color='w', yerr=stderr_values,
+p1 = plt.bar(ind + width * 1, mean_values, width, color='w',
+             yerr=stderr_values, ecolor='r',
              edgecolor='black', hatch=PATTERN[1])
 
 mean_values = []
 stderr_values = []
 get_results(CS_DIR, CS_CLIENT_NUMBERS, mean_values, stderr_values)
-p2 = plt.bar(ind + width * 2, mean_values,width, color='w', yerr=stderr_values,
+p2 = plt.bar(ind + width * 2, mean_values,width, color='w', yerr=stderr_values, ecolor='r',
              edgecolor='black', hatch=PATTERN[2])
 
 
 mean_values = []
 stderr_values = []
 get_results(SH_DIR, SH_CLIENT_NUMBERS, mean_values, stderr_values)
-p3 = plt.bar(ind + width * 3,  mean_values,width, color='w', yerr=stderr_values,
+p3 = plt.bar(ind + width * 3,  mean_values,width, color='w',
+             yerr=stderr_values, ecolor='r',
              edgecolor='black', hatch=PATTERN[3])
 
 mean_values = []
 stderr_values = []
 get_results(AC_DIR, AC_CLIENT_NUMBERS, mean_values, stderr_values, True)
-p4 = plt.bar(ind + width * 4,  mean_values,width, color='w', yerr=stderr_values,
+p4 = plt.bar(ind + width * 4,  mean_values,width, color='w',
+             # yerr is for errorbar & ecolor is for errobar colour
+             yerr=stderr_values, ecolor='r',
              edgecolor='black', hatch=PATTERN[4])
 
 
-# this is the bottom part of the rejection
-mean_values = []
-stderr_values = []
-get_results(AC_DIR, AC_CLIENT_NUMBERS, mean_values, stderr_values, True)
-p4 = plt.bar(ind + width * 5,  mean_values,width, color='w', yerr=stderr_values,
-             edgecolor='black', hatch=PATTERN[4])
+# IMPORTANT: commenting out the 'DLC + DLS + AC' + 'REJECTION' bar because it
+# is confusing, explain this in the paper
+# # this is the bottom part of the rejection
+# mean_values = []
+# stderr_values = []
+# get_results(AC_DIR, AC_CLIENT_NUMBERS, mean_values, stderr_values, True)
+# p4 = plt.bar(ind + width * 5,  mean_values,width, color='w', yerr=stderr_values,
+#              edgecolor='black', hatch=PATTERN[4])
 
-mean_values_reject = []
-stderr_values_reject = []
-get_results(AC_DIR, AC_CLIENT_NUMBERS, mean_values_reject, stderr_values_reject, True)
-p5 = plt.bar(ind + width * 5,  mean_values_reject, width, color='w', yerr=stderr_values_reject,
-             edgecolor='black', hatch=PATTERN[5], bottom=mean_values)
+# mean_values_reject = []
+# stderr_values_reject = []
+# get_results(AC_DIR, AC_CLIENT_NUMBERS, mean_values_reject, stderr_values_reject, True)
+# p5 = plt.bar(ind + width * 5,  mean_values_reject, width, color='w', yerr=stderr_values_reject,
+#              edgecolor='black', hatch=PATTERN[5], bottom=mean_values)
 
 
 mean_values = []
 stderr_values = []
 rw_helper.get_rw_results(CS_RW_DIR, CS_RW_CLIENT_NUMBERS, mean_values, stderr_values)
 print len(mean_values)
-p6 = plt.bar(ind[:2] + width * 6,  mean_values, width, color='w', yerr=stderr_values,
+p6 = plt.bar(ind[:2] + width * 5.3,  mean_values, width, color='w',
+             yerr=stderr_values, ecolor='r',
              edgecolor='black', hatch=PATTERN[6])
 
 
 mean_values = []
 stderr_values = []
 rw_helper.get_rw_results(SH_RW_DIR, SH_RW_CLIENT_NUMBERS, mean_values, stderr_values)
-p7 = plt.bar(ind[:2] + width * 7,  mean_values, width, color='w', yerr=stderr_values,
-             edgecolor='black', hatch=PATTERN[7])
+p7 = plt.bar(ind[:2] + width * 6.3,  mean_values, width, color='w',
+             yerr=stderr_values, ecolor='r',
+             edgecolor='black', hatch=PATTERN[5])
 
 
 
 
+plt.xlim(xmax=3.3)
 plt.ylabel('Deadline Miss Rate in %')
 plt.xlabel('Number of Concurrent Clients')
 plt.xticks(ind+width * 3.5, ('96', '192', '384', '768') )
-plt.yticks(np.arange(0,101,10))
-plt.legend((p0[0], p1[0], p2[0], p3[0], p4[0], p5[0], p6[0], p7[0]),
-           ('Storage System', 'Memcached',
-            'DLC', 'DLC + DLS', "DLC + DLS + AC",
-            "Overall Rejection Rate", 'DLC with 10% Write', 'DLS with 10% Write'),
-           loc = 'upper left', prop={'size':12})
+plt.yticks(np.arange(0,31,10))
+plt.legend((p0[0], p1[0], p2[0], p3[0], p4[0], p6[0], p7[0]),
+           ('Storage System',
+            'Memcached',
+            'DLC',
+            'DLC + DLS',
+            "DLC + DLS + AC",
+            'DLC with 10% Write',
+            'DLC + DLS with 10% Write'),
+           loc = 'upper right', prop={'size':12})
 
 # plt.show()
 saver.save(plt, 'EC2_BAR/miss_bar')
