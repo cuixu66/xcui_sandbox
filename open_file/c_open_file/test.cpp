835e08abca2d749ada0ee4b31f45c96da70caf26
cuixu <cuixu66@hotmail.com>
2015-04-28 15:27:49 -0400

    Made the outputs of open file test more informative

--- open_file/c_open_file/test.cpp
+++ open_file/c_open_file/test.cpp
@@ -7,20 +7,23 @@
 
 using namespace std;
 int main(){
-  int open_file = 665328 + 10000;
-  ifstream* myfile[open_file];;
-  for(int i = 0; i < open_file; i += 1){
-    myfile[i] = new ifstream();
-    myfile[i]->open("open_file");
-    if(i < open_file){
-      if(myfile[i]->is_open()){
-        cout << i << " ";
-      } else {
-        cout << "open file error" << endl;
-        cout << errno << endl;
-        exit(1);
-      }
+    int open_file = 665328 + 10000;
+    ifstream* myfile[open_file];;
+    for(int i = 0; i < open_file; i += 1){
+        myfile[i] = new ifstream();
+        myfile[i]->open("open_file");
+        if(i < open_file){
+            if(myfile[i]->is_open()){
+                cout << i << " ";
+            } else {
+                cout << endl << endl << "Sorry! Test failed!" << endl;
+                cout << "open file error" << endl;
+                cout << "errno:" << errno << endl;
+                exit(1);
+            }
+        }
     }
-  }
-  return 0;
+    cout << endl << endl << "Great! Test passed! " << open_file << endl;
+    cout << "# of open files allowed in the system is greater than " << open_file << endl;
+    return 0;
 }
