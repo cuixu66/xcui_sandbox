39eed925305604e366282e8a7eecfff376f1172c
xcui <cuixu66@hotmail.com>
2015-06-08 05:58:16 -0400

    Added last modified at date to MicroFuge webpage

--- xcui_site/projects/microfuge/js/fancy.js
+++ xcui_site/projects/microfuge/js/fancy.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-  // TODO
+  function myFunction() {
+    var x = document.lastModified;
+    document.getElementById("last_modified_at").innerHTML = "&nbsp; last modified at " + x;
+  }
+  myFunction();
 });
